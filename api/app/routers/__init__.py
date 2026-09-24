5e859882a379ed46b2dc77485787ca1f15765e31
Aaron John Schlosser, Ph.D. <ajschlosser@users.noreply.github.com>
2026-09-23 23:00:00 -0700

    Export vector store router

--- api/app/routers/__init__.py
+++ api/app/routers/__init__.py
@@ -5,6 +5,7 @@ from .annotations import router as annotations_router
 from .auth import router as auth_router
 from .i18n import router as i18n_router
 from .llm import router as llm_router
+from .stores import router as stores_router
 from .system import router as system_router
 
 __all__ = [
@@ -12,5 +13,6 @@ __all__ = [
     "auth_router",
     "i18n_router",
     "llm_router",
+    "stores_router",
     "system_router",
 ]
