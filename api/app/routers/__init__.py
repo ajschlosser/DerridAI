7d483d85f2c51ef51c4f2c960661c1937d2dc136
Aaron John Schlosser, Ph.D. <ajschlosser@users.noreply.github.com>
2026-09-23 22:52:13 -0700

    Add API router package

---
+++ api/app/routers/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Aaron John Schlosser, PhD.
+"""HTTP router exports for application composition."""
+
+from .annotations import router as annotations_router
+from .auth import router as auth_router
+from .i18n import router as i18n_router
+
+__all__ = ["annotations_router", "auth_router", "i18n_router"]
