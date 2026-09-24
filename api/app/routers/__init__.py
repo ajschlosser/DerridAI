5bcf28dce6c02cb6727aa27cfe5553df446dbae4
Aaron John Schlosser, Ph.D. <ajschlosser@users.noreply.github.com>
2026-09-23 22:57:17 -0700

    Export system and LLM routers

--- api/app/routers/__init__.py
+++ api/app/routers/__init__.py
@@ -4,5 +4,13 @@
 from .annotations import router as annotations_router
 from .auth import router as auth_router
 from .i18n import router as i18n_router
+from .llm import router as llm_router
+from .system import router as system_router
 
-__all__ = ["annotations_router", "auth_router", "i18n_router"]
+__all__ = [
+    "annotations_router",
+    "auth_router",
+    "i18n_router",
+    "llm_router",
+    "system_router",
+]
