91232e76d549a3efeb715edfd64dddc7521ac6c5
Aaron John Schlosser, Ph.D. <ajschlosser@users.noreply.github.com>
2026-09-23 23:07:54 -0700

    Export background job router

--- api/app/routers/__init__.py
+++ api/app/routers/__init__.py
@@ -4,6 +4,7 @@
 from .annotations import router as annotations_router
 from .auth import router as auth_router
 from .i18n import router as i18n_router
+from .jobs import router as jobs_router
 from .llm import router as llm_router
 from .records import router as records_router
 from .stores import router as stores_router
@@ -13,6 +14,7 @@ __all__ = [
     "annotations_router",
     "auth_router",
     "i18n_router",
+    "jobs_router",
     "llm_router",
     "records_router",
     "stores_router",
