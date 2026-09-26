c9f6830afc28cac0d68a381f12edffc4079a6835
Aaron John Schlosser, Ph.D. <ajschlosser@users.noreply.github.com>
2026-09-25 20:10:11 -0700

    style(corpus): shorten enrichment summary bindings

--- web/src/components/corpus-builder/CorpusEnrichmentConfiguration.vue
+++ web/src/components/corpus-builder/CorpusEnrichmentConfiguration.vue
@@ -52,8 +52,8 @@ const advancedOpen = ref(false);
       <span
         ><b>{{ i18n.t("pdf_corpus.llm_enrichment_title") }}</b
         ><small
-          >{{ props.selectedProviderLabel
-          }}<template v-if="props.selectedProfileModel"> · {{ props.selectedProfileModel }}</template>
+          >{{ selectedProviderLabel
+          }}<template v-if="selectedProfileModel"> · {{ selectedProfileModel }}</template>
           ·
           {{
             enrichmentMode === "deep"
